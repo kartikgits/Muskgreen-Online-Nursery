c357a81003370fa1b97a8711a809df411dae57d6
kartikgits <kartik.kapparwan1@gmail.com>
2019-10-13 13:42:48 +0530

    Add SignOut page

--- productDetail.php
+++ productDetail.php
@@ -1,5 +1,6 @@
 <?php
     require 'config.php';
+    session_start();
 
     if (!isset($_GET['proid'])) {
         header("Location: https://muskgreen.live");
--- products.php
+++ products.php
@@ -1,5 +1,6 @@
 <?php
 	require 'config.php';
+	session_start();
 ?>
 
 <!DOCTYPE html>
