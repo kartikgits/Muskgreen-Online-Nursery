2151ad2f42a9fce7f78af85a0770c0c26d551ac0
kartikgits <kartik.kapparwan1@gmail.com>
2019-10-22 09:44:45 +0530

    Make error page

---
+++ error.php
@@ -0,0 +1,53 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Error | MuskGreen - Buy Plants, Vegetables and Fruits online in Dehradun</title>
+	<meta name="robots" content="noindex">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="bootstrap4/css/bootstrap-grid.min.css" >
+	<link rel="stylesheet" href="bootstrap4/css/bootstrap.min.css" >
+    <link rel="stylesheet" type="text/css" href="css/errorStyle.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+                <h1>Oops!</h1>
+
+                <div class="card text-center" style="">
+                  <img src="extras/errorDesert.jpg" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <p class="card-text lead">Something went wrong or you landed at wrong page.</p>
+                  </div>
+                </div>
+
+                <div class="error-details">
+                    <small>
+                        <?php
+                            if (isset($_GET['errorMessage'])) {
+                                echo $_GET['errorMessage'];
+                            } else{
+                                echo "MuskGreen | Buy Plants, Vegetables, Fruits online";
+                            }
+                        ?>
+                    </small>
+                </div>
+                <div class="error-actions">
+                    <a href="index.php" class="btn btn-success btn-lg"><span class="fa fa-home"></span>
+                        Make It Green </a><a href="index.php" class="btn btn-default btn-lg"><span class="fa fa-pencil"></span> Contact Support </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
