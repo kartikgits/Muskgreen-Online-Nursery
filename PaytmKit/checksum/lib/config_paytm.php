75ea66bde21f76aca6250bfdf7f83442deea33bf
kartikgits <kartik.kapparwan1@gmail.com>
2019-11-09 23:40:07 +0530

    Paytm keys entry

--- PaytmKit/checksum/lib/config_paytm.php
+++ PaytmKit/checksum/lib/config_paytm.php
@@ -1,4 +1,4 @@
 <?php
 //Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
-define('PAYTM_MERCHANT_KEY', 'r@NBZexHE7d_ujT#'); 
+define('PAYTM_MERCHANT_KEY', 'jW4lWd5#rr7v0cCK'); 
 ?>
